a0bd7830f55ced1c9f9799a9cc0b12d76659ff74
Barry Hood <BarryPH@protonmail.com>
2017-07-12 11:40:24 +1200

    Set up server with passport authentication

---
+++ server/config/passport.js
@@ -0,0 +1,79 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const Project = mongoose.model('Project');
+
+const returnErrorMessage = (done, message) => done(null, false, { message });
+
+async function findUserByEmail(email) {
+	const caseInsensitiveEmail = new RegExp('^' + req.body.email + '$', 'i');
+	const user = await User.model.findOne({ email: caseInsensitiveEmail });
+
+	return user;
+}
+
+async function comparePasswords(passwordOne, passwordTwo) {
+	const doPasswordsMatch = await bcrypt.compare(passwordOne, passwordTwo);
+
+	return passwordsMatch;
+}
+
+async function createUser({ email, name, password }) {
+	const hash = await bcrypt.hash(password, 10);
+	const user = new User.model({
+		email: req.body.email,
+		password: hash,
+	});
+
+	user.save();
+
+	return user;
+}
+
+passport.serializeUser((user, done) => done(null, user.email));
+passport.deserializeUser(async (email, done) => done(null, await findUserByEmail(email)));
+
+passport.use('local-signin', new LocalStrategy({
+	usernameField: 'email',
+	passwordField: 'password',
+	passReqToCallback: true,
+}, async (req, email, password, done) => {
+	const invalidCrednentialsMessage = 'Invalid email or password';
+	const [user, validPassword] = await Promise.all([
+		findUserByEmail(email),
+		comparePasswords(password, user.password),
+	]);
+
+	if (!user || !isPasswordCorrect) {
+		return done(null, false, { message: invalidCrednentialsMessage });
+	}
+
+	done(null, user);
+}));
+
+passport.use('local-register', new LocalStrategy({
+	usernameField: 'email',
+	passwordField: 'password',
+	passReqToCallback: true,
+}, async (req, email, password, done) => {
+	const existingUser = await findUserByEmail(email);
+	if (existingUser) return done(null, false, { message: 'Email is already in use by another account'});
+
+	req.checkBody('email', 'A valid email is required.').isEmail();
+	req.checkBody('password', 'Password must be at least 8 characters long.').isLength({ min: 8 });
+	req.checkBody('password', 'Passwords do not match').equals(req.body.password2);
+	req.checkBody('name', 'Name must not be empty.').notEmpty();
+
+	const validationErrors = req.validationErrors();
+	if (validationErrors) return done(null, false, { message: validationErrors[0].msg});
+
+	const user = await createUser({
+		email,
+		name: req.body.name,
+		password,
+	});
+
+	done(null, user);
+}));
---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const expressValidator = require('express-validator');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+
+const pkg = require('./package.json');
+const app = express();
+const rootRequire = require.main.require;
+
+process.on('unhandledRejection', (reason) => {
+	console.log('Unhandled Rejection:', reason);
+});
+
+if (process.env.NODE_ENV !== 'prodution' && process.env.NODE_ENV !== 'test') {
+	app.use(logger('dev'));
+}
+
+mongoose.connect('mongodb://localhost/deloop', { useMongoClient: true });
+require('./models');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(passport.initialize());
+app.use(passport.session());
+rootRequire('./config/passport');
+
+async function main() {
+	const port = process.env.PORT || 3000;
+	await app.listen(port);
+
+	if (process.env.NODE_ENV !== 'test') {
+		console.log(`Server is running on port: ${port}`);
+	}
+}
+
+main();
