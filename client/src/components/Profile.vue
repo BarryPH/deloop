1f68970d80a4e769073a38e377fe1f56a45587d9
Barry Hood <BarryPH@protonmail.com>
2017-07-22 09:55:02 +1200

    Create settings page layout

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,45 +1,63 @@
 <script>
 import Projects from '@/components/shared/Projects.vue'
+import auth from '@/auth';
 
 export default {
 	name: 'profile-page',
 	components: {
 		Projects,
 	},
+	data() {
+		return {
+			auth,
+		}
+	},
 }
 </script>
 
 <template>
 	<div class='container'>
-		<div class='panel panel--bordered user-sidebar'>
-			<div class='text-center'>
-				<img class='headshot' src='http://creatingrealities.com/wp-content/uploads/2017/02/Loveleen-Kaur-Kang-Headshot-Square-300x300.jpg'>
-				<header class='user-name'>Thomas Terry</header>
-			</div>
-
-			<div class='user-info'>
-				<div>Projects: 24</div>
-				<div>Joined: 12/05/17</div>
-				<hr>
-				<div>Code Completion</div>
-				<div><a class='link' href='http://localhost:3000'>Website</a></div>
-			</div>
+		<div v-if='auth.user.authenticated' class='settings-link'>
+			<router-link to='/settings'>Edit settings &rarr;</router-link>
 		</div>
 
-		<div class='panel content'>
-			<div class='tabs'>
-				<a href='#' class='tab active'>
-					Projects
-				</a>
+		<div class='panels-wrapper'>
+			<div class='panel panel--bordered user-sidebar'>
+				<div class='text-center'>
+					<img class='headshot' src='http://creatingrealities.com/wp-content/uploads/2017/02/Loveleen-Kaur-Kang-Headshot-Square-300x300.jpg'>
+					<header class='user-name'>Thomas Terry</header>
+				</div>
+
+				<div class='user-info'>
+					<div>Projects: 24</div>
+					<div>Joined: 12/05/17</div>
+					<hr>
+					<div>Code Completion</div>
+					<div><a class='link' href='http://localhost:3000'>Website</a></div>
+				</div>
 			</div>
 
-			<Projects clear />
+			<div class='panel content'>
+				<div class='tabs'>
+					<a href='#' class='tab active'>
+						Projects
+					</a>
+				</div>
+
+				<Projects clear />
+			</div>
 		</div>
 	</div>
 </template>
 
 <style scoped>
-.container {
+.settings-link {
+	margin-bottom: 1rem;
+	opacity: 0.8;
+	text-align: right;
+}
+
+.panels-wrapper {
 	display: flex;
 	flex-wrap: wrap;
 	align-items: flex-start;
---
+++ client/src/components/Settings.vue
@@ -0,0 +1,57 @@
+<script>
+import axios from 'axios';
+
+export default {
+	name: 'settings-page',
+	data() {
+		return {
+			name: '',
+			email: '',
+			submtited: false,
+			info: {
+				success: false,
+				message: '',
+			},
+		}
+	},
+	methods: {
+		async handleSubmit() {
+			console.log('submitted');
+		}
+	}
+}
+</script>
+
+<template>
+	<div class='container--md'>
+		<div class='panel panel--bordered'>
+			<h3>Settings</h3>
+
+			<form v-on:submit.prevent='handleSubmit'>
+				<div v-show='submitted' v-bind:class='["formMessage", info.success ? "successMessage" : "errorMessage"]'>{{info.message}}</div>
+
+				<div class='form-group'>
+					<input placeholder='Full Name' name='name' v-model='name'>
+					<input placeholder='Email' name='email' v-model='email'>
+					<router-link to='/reset-password' class='password-reset'>Reset password &rarr;</router-link>
+				</div>
+
+				<button class='fill fill-go' type='submit'>Save</button>
+			</form>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+form {
+	margin-top: 2rem;
+}
+
+.formMessage {
+	margin-bottom: 2rem;
+}
+
+.password-reset {
+	color: var(--color-primary-shade);
+}
+</style>
