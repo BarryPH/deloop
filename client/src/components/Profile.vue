55145d5ebf1843d2c61fad570d4a218063b17191
Barry Hood <BarryPH@protonmail.com>
2017-07-26 11:17:34 +1200

    Combine Profile page and shared component

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,16 +1,24 @@
 <script>
-import Profile from '@/components/shared/Profile.vue';
+import Projects from '@/components/shared/Projects.vue';
 import auth from '@/auth';
 
 export default {
 	name: 'profile-page',
 	components: {
-		Profile,
+		Projects,
 	},
 	data() {
 		return {
 			auth,
 		};
+	},
+	computed: {
+		user() {
+			return this.$store.state.users[auth.user.id];
+		},
+	},
+	async created() {
+		this.$store.dispatch('FETCH_USER', { id: auth.user.id });
 	},
 };
 </script>
@@ -21,7 +29,32 @@ export default {
 			<router-link to='/settings'>Edit settings &rarr;</router-link>
 		</div>
 
-		<Profile />
+		<div v-if='user' class='panels-wrapper'>
+			<div class='panel panel--bordered user-sidebar'>
+				<div class='text-center'>
+					<img class='headshot' src='http://creatingrealities.com/wp-content/uploads/2017/02/Loveleen-Kaur-Kang-Headshot-Square-300x300.jpg'>
+					<header class='user-name'>{{user.name}}</header>
+				</div>
+
+				<div class='user-info'>
+					<div>Projects: {{user.projectCount}}</div>
+					<div>Joined: {{user.joined}}</div>
+					<hr>
+					<div>{{user.business}}</div>
+					<div><a class='link' href='http://localhost:3000'>{{user.website}}</a></div>
+				</div>
+			</div>
+
+			<div class='panel content'>
+				<div class='tabs'>
+					<a href='#' class='tab active'>
+						Projects
+					</a>
+				</div>
+
+				<Projects :author='$route.params.id' clear />
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -30,5 +63,60 @@ export default {
 	margin-bottom: 1rem;
 	opacity: 0.8;
 	text-align: right;
+}
+
+.panels-wrapper {
+	display: flex;
+	flex-wrap: wrap;
+	align-items: flex-start;
+}
+
+.user-sidebar {
+	padding: 2rem;
+	margin-right: 0.3rem;
+	width: 250px;
+}
+
+.user-sidebar .user-name {
+	margin-top: 0.8rem;
+	font-weight: bold;
+}
+
+.headshot {
+	width: 125px;
+	height: 125px;
+	border-radius: 100%;
+}
+
+.user-info {
+	margin-top: 1.5rem;
+	font-size: 0.88rem;
+}
+
+.user-info hr {
+	margin: 1rem 0;
+}
+
+
+.content {
+	flex-grow: 1;
+	flex-basis: 0;
+}
+
+.content.panel {
+	padding: 0;
+}
+
+.tabs {
+	border-bottom: 1px solid #eaeaea;
+}
+
+.tab {
+	display: inline-block;
+	padding: 1rem 1.5rem 1rem;
+}
+
+.tab.active {
+	font-weight: bold;
 }
 </style>
